030630fe4eaedb81ca7ec3ab7483fe0dbfbd6e20
Sekina <Sekina@PC-Sekina>
2017-06-02 12:48:00 +0200

    Creation de la classe Coord

--- src/Engine/Entite.java
+++ src/Engine/Entite.java
@@ -1,9 +1,9 @@
 package Engine;
 
 public interface Entite {
-	
+
 	void detruire();
-	
+
 	void apparaitre();
 
 }
--- src/Engine/Operateurs.java
+++ src/Engine/Operateurs.java
@@ -5,22 +5,22 @@ public class Operateurs implements Entite {
 	@Override
 	public void detruire() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void apparaitre() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	void stock(Personnages p){
-		//TODO 
+
+	void stock(Personnages p) {
+		// TODO
 	}
-	
-	/*default action : straight forward*/
-	void action(){
-		//TODO 
+
+	/* default action : straight forward */
+	void action() {
+		// TODO
 	}
 
 }
