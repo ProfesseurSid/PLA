85fc60c27a9089f28377dd8167de6228c0d7b79a
Sekina <Sekina@PC-Sekina>
2017-06-02 12:43:10 +0200

    Creation de la classe Coord

--- src/Engine/Personnages.java
+++ src/Engine/Personnages.java
@@ -2,25 +2,24 @@ package Engine;
 
 public class Personnages implements Vivante {
 
-	Operateurs Inventory [];
+	Operateurs Inventory[];
 	int x, y;
-	
+
 	@Override
 	public void detruire() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void apparaitre() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void mouvement(card p, int nb) {
+	public void mouvement(Coord p, int nb) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
--- src/Engine/Robots.java
+++ src/Engine/Robots.java
@@ -4,23 +4,23 @@ public class Robots implements Vivante {
 
 	Automates behavior;
 	int x, y;
-		
+
 	@Override
 	public void detruire() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void apparaitre() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void mouvement(card p, int nb) {
+	public void mouvement(Coord p, int nb) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
